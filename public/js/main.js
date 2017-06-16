409928b52fe32b6431566c539c91a9b944053355
Levi Zitting <levizitting5663@gmail.com>
2017-06-16 18:35:36 -0500

    More changes to admin and finishing projects page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.modal').modal();
+    $('.carousel').carousel({
+        fullWidth: true,
+        indicators: true
+    });
 });
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -3,4 +3,8 @@ declare var $;
 $(document).ready(() => {
 	$(".button-collapse").sideNav();
 	$('.modal').modal();
+	$('.carousel').carousel({
+		fullWidth: true,
+		indicators: true
+	});
 });
