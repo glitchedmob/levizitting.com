8d1395a66f2e3b266c9101a559bfb0a6b41fc703
Levi Zitting <levizitting5663@gmail.com>
2017-06-22 18:29:21 -0500

    Adding 404 page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Levi Zitting - Programmer, Creator, and Life Long Learner</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="/css/app.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#1976D2">
+</head>
+<body>
+
+<div class="blue darken-2 valign-wrapper" id="error-404">
+    <div class="container white-text center-align">
+        <h1>404</h1>
+        <h4>Whoops, that's an error</h4>
+        <h5>Maybe you were looking for some of these pages</h5>
+        <div class="row align-center cta-buttons">
+            <div class="col s12 m6 l3 offset-l3"><a class="btn waves-effect" href="blog.html">Home</a></div>
+            <div class="col s12 m6 l3"><a class="btn waves-effect" href="projects.html">Blog</a></div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="/js/app.js"></script>
+</body>
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="/css/admin.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#1976D2">
 </head>
 <body>
 <div class="blue darken-2 valign-wrapper" id="login">
