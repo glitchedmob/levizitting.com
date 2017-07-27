6540ab8ffc3c78949bb19e0ce4fb2e74c137e9ba
Levi Zitting <levizitting5663@gmail.com>
2017-07-27 11:06:42 -0500

    Updating home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,22 @@
     <div class="valign-wrapper" id="home-header">
         <div class="container">
             <div class="row">
-                <div class="col s10 offset-s1">
-                    <div class="card white center-align" id="home-profile-card"><img id="home-profile-img" src="images/profile.jpg">
-                        <h1>Levi Zitting</h1>
-                        <p>I am a programmer, creator, and life long learner</p>
-                        <p class="flow-text">Welcome to my website.<br>While you're here check out some of the stuff I've been working on</p>
-                        <div class="row align-center cta-buttons">
-                            <div class="col s12 m6 l3 offset-l3"><a class="btn waves-effect" href="/blog">Blog</a></div>
-                            <div class="col s12 m6 l3"><a class="btn waves-effect" href="/projects">Projects</a></div>
+                <div class="col l12">
+                    <div class="card white center-align" id="home-profile-card">
+                        <div class="row">
+                            <div class="col s12 m6">
+                                <img id="home-profile-img" src="images/profile.jpg">
+                                <h1>Levi Zitting</h1></div>
+                            <div class="col s12 m6">
+                                <p>I am a programmer, creator, and life long learner</p>
+                                <p>Welcome to my website.<br>While you're here check out some of the stuff I've been working on</p>
+                                <div class="cta-buttons">
+                                    <div class="row">
+                                        <div class="col s12 m12 l6"><a class="btn waves-effect" href="/blog">Blog</a></div>
+                                        <div class="col s12 m12 l6"><a class="btn waves-effect" href="/projects">Projects</a></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
