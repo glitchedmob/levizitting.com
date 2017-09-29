f383a34200849d2846250df997113623eb428fea
Levi Zitting <levizitting5663@gmail.com>
2017-09-28 20:27:56 -0500

    Updating homepage responsiveness

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,18 +6,16 @@
             <div class="row">
                 <div class="col l12">
                     <div class="card white center-align" id="home-profile-card">
-                        <div class="row">
-                            <div class="col s12 m6">
-                                <img id="home-profile-img" src="images/profile.jpg">
-                                <h1>Levi Zitting</h1></div>
-                            <div class="col s12 m6">
-                                <p>I am a programmer, creator, and life long learner</p>
-                                <p>Welcome to my website.<br>While you're here check out some of the stuff I've been working on</p>
-                                <div class="cta-buttons">
-                                    <div class="row">
-                                        <div class="col s12 m12 l6"><a class="btn waves-effect" href="/blog">Blog</a></div>
-                                        <div class="col s12 m12 l6"><a class="btn waves-effect" href="/projects">Projects</a></div>
-                                    </div>
+                        <div class="header">
+                            <img src="/images/profile.jpg">
+                            <h1>Levi Zitting</h1></div>
+                        <div class="description">
+                            <p>I am a programmer, creator, and life long learner</p>
+                            <p>Welcome to my website.<br>While you're here check out some of the stuff I've been working on</p>
+                            <div class="cta-buttons">
+                                <div class="row">
+                                    <div class="col s12 m12 l6"><a class="btn waves-effect" href="/blog">Blog</a></div>
+                                    <div class="col s12 m12 l6"><a class="btn waves-effect" href="/projects">Projects</a></div>
                                 </div>
                             </div>
                         </div>
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -3,22 +3,24 @@
 @section("content")
     <div class="container">
         <div class="row">
-            @foreach($projects as $project)
-                <div class="col m6">
-                    <div class="card small">
-                        <div class="card-image"><img src="/images/projects/{{ $project->image }}"><span class="card-title">{{ $project->title }}</span></div>
-                        <div class="card-content">
-                            <p>{{ $project->description }}</p>
-                        </div>
-                        <div class="card-action"><a href="#" class="modal-trigger waves-effect center-align teal-text lighten-2-text" data-target="modal{{ $project->id }}">Learn More</a></div>
-                        <!-- Modal Structure-->
-                        <div class="modal modal-fixed-footer" id="modal{{ $project->id }}">
-                            <div class="modal-content project-content">{!! $project->body !!}</div>
-                            <div class="modal-footer"><a class="modal-action modal-close waves-effect btn-flat" href="#!">Close</a></div>
+            <div class="col m10 offset-m1">
+                @foreach($projects as $project)
+                    <div class="col m6">
+                        <div class="card small">
+                            <div class="card-image"><img src="/images/projects/{{ $project->image }}"><span class="card-title">{{ $project->title }}</span></div>
+                            <div class="card-content">
+                                <p>{{ $project->description }}</p>
+                            </div>
+                            <div class="card-action"><a href="#" class="modal-trigger waves-effect center-align teal-text lighten-2-text" data-target="modal{{ $project->id }}">Learn More</a></div>
+                            <!-- Modal Structure-->
+                            <div class="modal modal-fixed-footer" id="modal{{ $project->id }}">
+                                <div class="modal-content project-content">{!! $project->body !!}</div>
+                                <div class="modal-footer"><a class="modal-action modal-close waves-effect btn-flat" href="#!">Close</a></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
