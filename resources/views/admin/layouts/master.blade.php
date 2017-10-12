4af410ec44a1a32e5ae96cfc7687d20f273b39e2
Levi Zitting <levizitting5663@gmail.com>
2017-10-12 10:44:39 -0500

    Adding all known browser navigation themeing options

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -6,6 +6,10 @@
     <link type="text/css" rel="stylesheet" href="/css/admin.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#1976D2">
+    <meta name="msapplication-navbutton-color" content="#1976D2">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1976D2">
 </head>
 <body>
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,10 @@
     <link type="text/css" rel="stylesheet" href="/css/app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#1976D2">
+    <meta name="msapplication-navbutton-color" content="#1976D2">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1976D2">
     <meta property="og:title" content="Levi Zitting - Programmer, Creator, and Life Long Learner"/>
     <meta property="og:image" content="http://levizitting.com/images/share.jpg"/>
     <meta property="og:site_name" content="Levi Zitting - Programmer, Creator, and Life Long Learner"/>
