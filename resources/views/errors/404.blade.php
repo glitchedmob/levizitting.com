5117c7ea2595b18e3666b878462f5e3aa8622d6a
Levi Zitting <levizitting5663@gmail.com>
2017-10-16 11:56:23 -0500

    Updating 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>Levi Zitting - Programmer, Creator, and Life Long Learner</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="/css/app.css">
+    <link type="text/css" rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#1976D2">
 </head>
@@ -14,13 +14,16 @@
         <h1>404</h1>
         <h4>Whoops, that's an error</h4>
         <h5>Maybe you were looking for some of these pages</h5>
-        <div class="row align-center cta-buttons">
-            <div class="col s12 m6 l3 offset-l3"><a class="btn waves-effect" href="/">Home</a></div>
-            <div class="col s12 m6 l3"><a class="btn waves-effect" href="/blog">Blog</a></div>
-        </div>
+        <a class="btn waves-effect" href="/">Home</a>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <a class="btn waves-effect" href="/blog">Blog</a>
     </div>
 </div>
 
-<script type="text/javascript" src="/js/app.js"></script>
+<script type="text/javascript" src="{{ mix('/js/manifest.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/vendor.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
