b23ebd7301bcd4782ccddab916630c928069967e
Levi Zitting <levizitting5663@gmail.com>
2017-10-19 12:07:36 -0500

    Updates to the contact submit page

--- resources/views/contact-submit.blade.php
+++ resources/views/contact-submit.blade.php
@@ -4,13 +4,14 @@
     <div class="valign-wrapper" id="contact-thank-you">
         <div class="container">
             <div class="row">
-                <div class="col s12 m6 offset-m3 card">
-                    <h1 class="center-align">Thank You</h1>
-                    <p class="center-align flow-text">I'll get back to you shortly</p>
-                    <div class="row center-align cta-buttons">
-                        <div class="col s12 m6"><a class="btn waves-effect" href="/">Home</a></div>
-                        <div class="col s12 m6"><a class="btn waves-effect" href="/blog">Blog</a></div>
-                    </div>
+                <div class="col s12 m6 offset-m3 card center-align">
+                    <h1>Thank You</h1>
+                    <p class="flow-text">I'll get back to you shortly</p>
+                    <a class="btn waves-effect" href="/">Home</a>
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <a class="btn waves-effect" href="/blog">Blog</a>
                 </div>
             </div>
         </div>
