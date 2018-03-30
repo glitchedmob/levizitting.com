b360f2a56a88d373ca805850b584060d97a107b9
Levi Zitting <levizitting5663@gmail.com>
2018-03-30 08:19:07 -0500

    Restructure routing and setup homepage component

---
+++ src/client/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
+
+export const appRoutingComponents = [ AboutComponent, ContactComponent, HomeComponent ];
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { routes } from './app.routing';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { appRoutingComponents, AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
+
 import { AppNavComponent } from './components/app-nav/app-nav.component';
 import { LogoComponent } from './components/logo/logo.component';
 
@@ -15,16 +13,15 @@ import { LogoComponent } from './components/logo/logo.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
     AppNavComponent,
     LogoComponent,
+    appRoutingComponents
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'nestJS' }),
     HttpClientModule,
-    RouterModule.forRoot(routes),
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   entryComponents: [
