d7aac40b51a637871292ebd741e4fce1300f5a6c
Levi Zitting <levizitting5663@gmail.com>
2018-03-30 16:48:33 -0500

    Add additional pages

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
   {
@@ -18,12 +20,21 @@ export const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
-  }
+  },
+  {
+    path: 'projects',
+    component: BlogComponent
+  },
+  {
+    path: 'blog',
+    component: ProjectsComponent
+  },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  declarations: [ProjectsComponent, BlogComponent]
 })
 export class AppRoutingModule { }
 
