d804aafd8e54a21a6b5b82c62c3040fe3c3412bf
Levi Zitting <levizitting5663@gmail.com>
2018-03-29 20:07:06 -0500

    Add initial angular and nest apps

---
+++ src/client/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { routes } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'nestJS' }),
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/server/server.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+import { ApiModule } from './modules/api/api.module';
+import { StaticModule } from './modules/static/static.module';
+
+@Module({
+  imports: [
+    ApiModule,
+    StaticModule,
+  ],
+  controllers: [],
+  components: [],
+})
+export class ApplicationModule {}
