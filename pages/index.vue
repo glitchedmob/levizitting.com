224a93df8d4e6ebcf11eda676bb0aacb35c7b26f
Levi Zitting <levizitting5663@gmail.com>
2018-04-21 00:34:48 -0500

    Resetup nuxt using yarn create

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,42 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <logo/>
       <h1 class="title">
         levizitting.com
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        My kryptonian Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    AppLogo
+    Logo
   }
 }
 </script>
 
 <style>
-.container {
+.container
+{
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-
-.title {
+.title
+{
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
@@ -49,16 +44,16 @@ export default {
   color: #35495e;
   letter-spacing: 1px;
 }
-
-.subtitle {
+.subtitle
+{
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
-.links {
+.links
+{
   padding-top: 15px;
 }
 </style>
