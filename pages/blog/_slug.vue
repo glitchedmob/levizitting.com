880e10add216f5b9688b96c658512651d908e313
Levi Zitting <levizitting5663@gmail.com>
2018-04-22 00:57:59 -0500

    Add markdown support for blog posts

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="blog-post main-content">
-
+  <div class="blog-post main-content" v-if="post">
+    <img :src="post.image" :alt="post.title" v-if="post.image">
+    <h3>{{ post.title }}</h3>
+    <p class="date">Published {{ post.date | humanDate }}</p>
+    <div class="body">
+      <vue-markdown>
+        {{ post.body }}
+      </vue-markdown>
+    </div>
   </div>
 </template>
 
@@ -9,13 +16,22 @@ import {
   Component,
   Vue
 } from "nuxt-property-decorator";
+import VueMarkdown from 'vue-markdown';
 
+import { humanDate } from '../../filters/date-filters'
 
-@Component({})
+@Component({
+  components: {
+    VueMarkdown
+  },
+  filters: {
+    humanDate
+  }
+})
 export default class extends Vue {
   async asyncData({ params }) {
     let post = await import('~/content/blog/posts/' + params.slug + '.json');
-    return post;
+    return { post };
   }
 }
 </script>
@@ -23,4 +39,14 @@ export default class extends Vue {
 <style lang="stylus" scoped>
   .blog-post
     margin 2rem 0
+
+    img
+      width 100%
+      margin 0 0 1rem 0
+
+  .body
+    margin 1rem 0 0 0
+
+    & /deep/
+      line-height 1.8rem
 </style>
