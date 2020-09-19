eb58a4609c530287961329344ebd0fd27d9433e3
Levi Zitting <me@levizitting.com>
2020-09-19 14:50:07 -0500

    Add noopener to external links

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,19 +1,44 @@
 <template>
     <footer class="footer">
         <div class="social">
-            <a href="https://github.com/glitchedmob" target="_blank" title="Github">
+            <a
+                href="https://github.com/glitchedmob"
+                target="_blank"
+                title="Github"
+                rel="noopener"
+            >
                 <i class="icon-github" aria-hidden="true"></i>
             </a>
-            <a href="https://dev.to/levi_zitting" target="_blank" title="Dev.to">
+            <a
+                href="https://dev.to/levi_zitting"
+                target="_blank"
+                title="Dev.to"
+                rel="noopener"
+            >
                 <i class="icon-dev" aria-hidden="true"></i>
             </a>
-            <a href="https://twitter.com/levizitting" target="_blank" title="Twitter">
+            <a
+                href="https://twitter.com/levizitting"
+                target="_blank"
+                title="Twitter"
+                rel="noopener"
+            >
                 <i class="icon-twitter" aria-hidden="true"></i>
             </a>
-            <a href="https://www.instagram.com/levi_zitting/" target="_blank" title="Instagram">
+            <a
+                href="https://www.instagram.com/levi_zitting/"
+                target="_blank"
+                title="Instagram"
+                rel="noopener"
+            >
                 <i class="icon-instagram" aria-hidden="true"></i>
             </a>
-            <a href="https://www.linkedin.com/in/levi-zitting/" target="_blank" title="Linkedin">
+            <a
+                href="https://www.linkedin.com/in/levi-zitting/"
+                target="_blank"
+                title="Linkedin"
+                rel="noopener"
+            >
                 <i class="icon-linkedin" aria-hidden="true"></i>
             </a>
             <a href="mailto:me@levizitting.com" title="Email">
