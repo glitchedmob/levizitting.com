8b54e89b4375ed6390ecceb6c7eff23f57f0edea
Levi Zitting <me@levizitting.com>
2023-12-02 20:24:05 -0600

    chore: cleanup entrypoint

--- LeviZitting.Com.Web/Program.cs
+++ LeviZitting.Com.Web/Program.cs
@@ -1,19 +1,13 @@
-namespace LeviZitting.Com.Web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args)
-                .Build()
-                .Run();
+using LeviZitting.Com.Web;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureUmbracoDefaults()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureUmbracoDefaults()
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStaticWebAssets();
+        webBuilder.UseStartup<Startup>();
+    });
+
+var app = builder.Build();
+
+app.Run();
