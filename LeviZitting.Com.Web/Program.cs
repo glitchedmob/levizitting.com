ba907da10313baed701ae02a8285c83451a3a9bf
Levi Zitting <me@levizitting.com>
2023-12-03 11:43:46 -0600

    chore: cleanup startup and launch options

--- LeviZitting.Com.Web/Program.cs
+++ LeviZitting.Com.Web/Program.cs
@@ -1,5 +1,3 @@
-using LeviZitting.Com.Web;
-
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureUmbracoDefaults()
     .ConfigureWebHostDefaults(webBuilder =>
@@ -11,3 +9,46 @@ var builder = Host.CreateDefaultBuilder(args)
 var app = builder.Build();
 
 app.Run();
+
+public class Startup
+{
+    private readonly IWebHostEnvironment _env;
+    private readonly IConfiguration _config;
+
+    public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+    {
+        _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddUmbraco(_env, _config)
+            .AddBackOffice()
+            .AddWebsite()
+            .AddDeliveryApi()
+            .AddComposers()
+            .Build();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseUmbraco()
+            .WithMiddleware(u =>
+            {
+                u.UseBackOffice();
+                u.UseWebsite();
+            })
+            .WithEndpoints(u =>
+            {
+                u.UseInstallerEndpoints();
+                u.UseBackOfficeEndpoints();
+                u.UseWebsiteEndpoints();
+            });
+    }
+}
