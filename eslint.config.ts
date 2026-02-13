ba63bb08177a691bf3971535dc161ecbfe054d5d
Levi Zitting <me@levizitting.com>
2026-02-13 01:11:53 -0600

    fix: conflicts between prettier and eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,4 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default withNuxt({
-    rules: {},
-});
+export default withNuxt(eslintConfigPrettier);
